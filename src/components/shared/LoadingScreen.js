67c468e53023bb700c3605ad793a8b5a26a668f0
theshivamgupta <shivam.189301018@muj.manipal.edu>
2021-04-04 18:33:12 +0530

    #Change: Changed LoadingLogo

--- src/components/shared/LoadingScreen.js
+++ src/components/shared/LoadingScreen.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { useLoadingScreenStyles } from "../../styles";
-import { LogoLoadingIcon } from "../../icons";
 
 function LoadingScreen() {
   const classes = useLoadingScreenStyles();
 
   return (
     <section className={classes.section}>
-      <span>
-        <LogoLoadingIcon />
-      </span>
+      <span style={{ fontSize: "70px", color: "grey" }}>Loading...</span>
     </section>
   );
 }
